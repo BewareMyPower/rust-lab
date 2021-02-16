bdf08b8e256c6dc9846a83c08b9fb33209541098
Yunze Xu <xyzinfernity@163.com>
2021-02-16 23:51:56 +0800

    Add initial minigrep project

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ The experimental projects when I study [Rust](https://www.rust-lang.org).
 ## Books
 
 - ["the book"](https://doc.rust-lang.org/book/title-page.html)
+
+## Projects
+
+- [minigrep](./minigrep): The example project from *the book* chaptor 12: An I/O Project: Building a Command Line Program.
---
+++ minigrep/src/lib.rs
@@ -0,0 +1,61 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+// See details in https://doc.rust-lang.org/book/ch17-00-oop.html
+// For now, `Box<dyn Error>` means the function will return a type that implements the `Error`
+// trait, but we don't have to specify what particular type the return value will be.
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
