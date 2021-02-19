1de5b45ef0e25ced10ff2a8592fcd37756b7de69
Yunze Xu <xyzinfernity@163.com>
2021-02-19 23:32:06 +0800

    Use iterator instead of copying env::args() to a vector

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::error::Error;
 use std::fs;
 
 pub struct Config {
@@ -9,13 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next(); // skip the executable path
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -27,10 +31,7 @@ impl Config {
     }
 }
 
-// See details in https://doc.rust-lang.org/book/ch17-00-oop.html
-// For now, `Box<dyn Error>` means the function will return a type that implements the `Error`
-// trait, but we don't have to specify what particular type the return value will be.
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), &'static str> {
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
