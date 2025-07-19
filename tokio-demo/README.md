1854540d1f2f89561aef99fc08991d5c1eebb068
Yunze Xu <xyzinfernity@163.com>
2025-07-19 20:05:21 +0800

    tokio-demo: Add subcommand to run client operations (GET or SET)

--- tokio-demo/README.md
+++ tokio-demo/README.md
@@ -14,7 +14,26 @@ cargo run --package tokio-demo server
 
 By default, it listens at `127.0.0.1:6379`, you can change it by the `--url` option.
 
-Run the client:
+Run a single client command:
+
+```bash
+cargo run --package tokio-demo client get key
+cargo run --package tokio-demo client set key value
+cargo run --package tokio-demo client get key
+```
+
+The outputs will be:
+
+```
+No value found for key "key"
+Set "key" => "value" in Redis
+Got value "value" for key "key"
+```
+
+Run the client example with composite commands, which:
+1. Set the `hello => world` key-value pair.
+2. Get the value of key `hello`.
+2. Get the value of key `key`.
 
 ```bash
 cargo run --package tokio-demo --example hello-redis
--- tokio-demo/src/main.rs
+++ tokio-demo/src/main.rs
@@ -1,7 +1,7 @@
 pub mod server;
 
 use clap::{Parser, Subcommand};
-use mini_redis::Result;
+use mini_redis::{Result, client};
 
 #[derive(Parser)]
 #[command(version, about = "Mini Redis Server", long_about = None)]
@@ -19,7 +19,16 @@ enum Commands {
     Client {
         #[arg(long, default_value_t = String::from("127.0.0.1:6379"))]
         url: String,
+
+        #[command(subcommand)]
+        client_commands: Option<ClientCommands>,
     },
+}
+
+#[derive(Subcommand)]
+enum ClientCommands {
+    Get { key: String },
+    Set { key: String, value: String },
 }
 
 #[tokio::main]
@@ -27,7 +36,30 @@ async fn main() -> Result<()> {
     let cli = Cli::parse();
     match cli.command {
         Some(Commands::Server { url }) => server::run_server(&url).await,
-        Some(Commands::Client { .. }) => Err("client mode is not implemented yet".into()),
+        Some(Commands::Client {
+            url,
+            client_commands,
+        }) => match client_commands {
+            Some(ClientCommands::Get { key }) => {
+                let mut client = client::connect(&url).await?;
+                match client.get(&key).await? {
+                    Some(value) => {
+                        let string_value = String::from_utf8(value.to_vec()).unwrap();
+                        println!("Got value {:?} for key {:?}", string_value, key);
+                    }
+                    None => println!("No value found for key {:?}", key),
+                }
+                Ok(())
+            }
+            Some(ClientCommands::Set { key, value }) => {
+                let mut client = client::connect(&url).await?;
+                let value_clone = value.clone();
+                client.set(&key, value.into()).await?;
+                println!("Set {:?} => {:?} in Redis", key, value_clone);
+                Ok(())
+            }
+            None => Err("No client command provided. Use --help for usage information.".into()),
+        },
         None => Err("No command provided. Use --help for usage information.".into()),
     }
 }
