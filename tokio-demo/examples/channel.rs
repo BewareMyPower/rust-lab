1a29f3c4dab0410a040afd5e237c77f640219014
Yunze Xu <xyzinfernity@163.com>
2025-07-26 14:19:28 +0800

    tokio_demo: Receive response from client tasks

--- tokio-demo/examples/channel.rs
+++ tokio-demo/examples/channel.rs
@@ -1,11 +1,20 @@
 use bytes::Bytes;
-use mini_redis::client;
-use tokio::sync::mpsc;
+use mini_redis::{Result, client};
+use tokio::sync::{mpsc, oneshot};
+
+type Responder<T> = oneshot::Sender<Result<T>>;
 
 #[derive(Debug)]
 enum Command {
-    Get { key: String },
-    Set { key: String, value: Bytes },
+    Get {
+        key: String,
+        responder: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        value: Bytes,
+        responder: Responder<()>,
+    },
     Quit,
 }
 
@@ -17,17 +26,17 @@ async fn main() {
         let mut client = client::connect("127.0.0.1:6379").await.unwrap();
         while let Some(cmd) = rx.recv().await {
             match cmd {
-                Command::Get { key } => match client.get(&key).await.unwrap() {
-                    Some(bytes) => println!(
-                        "Got value for key {}: {:?}",
-                        key,
-                        String::from_utf8(bytes.to_vec()).unwrap()
-                    ),
-                    None => println!("No value found for key {}", key),
-                },
-                Command::Set { key, value } => {
-                    println!("Set key: {}, value: {:?}", &key, &value);
-                    client.set(&key, value).await.unwrap();
+                Command::Get { key, responder } => {
+                    let result = client.get(&key).await;
+                    let _ = responder.send(result);
+                }
+                Command::Set {
+                    key,
+                    value,
+                    responder,
+                } => {
+                    let result = client.set(&key, value).await;
+                    let _ = responder.send(result);
                 }
                 Command::Quit => {
                     println!("Shutting down manager");
@@ -39,20 +48,30 @@ async fn main() {
 
     let tx1 = tx.clone();
     let handle1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         tx1.send(Command::Get {
             key: String::from("foo"),
+            responder: resp_tx,
         })
         .await
         .unwrap();
+        match resp_rx.await.unwrap().unwrap() {
+            Some(bytes) => println!("Got value: {}", String::from_utf8(bytes.to_vec()).unwrap()),
+            None => println!("Key not found"),
+        }
     });
     let tx2 = tx.clone();
     let handle2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         tx2.send(Command::Set {
             key: String::from("foo"),
             value: "bar".into(),
+            responder: resp_tx,
         })
         .await
         .unwrap();
+        let result = resp_rx.await.unwrap().unwrap();
+        println!("Set foo => bar: {:?}", result);
     });
 
     handle1.await.unwrap();
